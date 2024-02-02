0b269b79f5ea4ded5b564a6a4ed325a525032d70
victortillett <123124794+victortillett@users.noreply.github.com>
2024-02-02 16:52:23 -0600

    Add files via upload

--- quiz1/main.go
+++ quiz1/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"text/template"
+	"time"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello this is quiz 1"))
+
+	weekday := time.Now().Weekday()
+
+	// Prints the current weekday
+	fmt.Println("Weekday is: ", weekday)
+}
+
 func displayDow(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Stranger ! Today is monday"))
+	day := time.Now().Weekday()
+
+	ts, _ := template.ParseFiles("display.day.page.tpml")
+	ts.Execute(w, day)
 }
 
 func main() {
 	// server mux
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
 	mux.HandleFunc("/dow", displayDow)
 
 	// mux.HandleFunc("/time", displayTime)
